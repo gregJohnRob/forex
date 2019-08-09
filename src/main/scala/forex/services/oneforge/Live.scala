6dc464ee842d8c7ebbcf0ffe9ff6f49e18db8e69
gregJohnRob <gregor_r@btinternet.com>
2019-08-09 18:23:35 +0100

    Printing the output of the api call

--- src/main/scala/forex/services/oneforge/Live.scala
+++ src/main/scala/forex/services/oneforge/Live.scala
@@ -1,28 +1,48 @@
 package forex.services.oneforge
 
-
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.stream.ActorMaterializer
 import forex.domain._
 import monix.eval.Task
 import org.atnos.eff._
 import org.atnos.eff.addon.monix.task._
 
+import scala.concurrent.ExecutionContextExecutor
+
 final class Live[R] private[oneforge] (apiKey: String)(
     implicit
     m1: _task[R]
 ) extends Algebra[Eff[R, ?]] {
+  import Live._
+
+  def quoteCall(pair: Rate.Pair) =
+    s"$URL$QUOTES${pair.from}${pair.to}&$API_KEY$apiKey"
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
   override def get(
       pair: Rate.Pair
-  ): Eff[R, Error Either Rate] =
+  ): Eff[R, Error Either Rate] = {
+    val apiCall = Task.defer {
+      val future = Http()
+        .singleRequest(HttpRequest(uri = quoteCall(pair)))
+          .map(println(_))
+      Task.fromFuture(future)
+    }
     for {
+      result1 ← fromTask(apiCall)
       result ← fromTask(Task.now(Rate(pair, Price(BigDecimal(100)), Timestamp.now)))
     } yield Right(result)
+  }
 }
 
 private[oneforge] object Live {
   val URL: String = "https://forex.1forge.com/1.0.3"
   val QUOTES: String = "/quotes?pairs="
   val API_KEY = "api_key="
-  def quoteCall(pair: Rate.Pair, apiKey: String) =
-    s"$URL$QUOTES${pair.from}${pair.to}&$API_KEY$apiKey"
 
 }
