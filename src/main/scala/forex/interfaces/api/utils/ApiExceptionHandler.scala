a412b6719870cc18e1213c3b81323f46021dc225
gregJohnRob <gregor_r@btinternet.com>
2019-08-12 19:22:43 +0100

    Sending HttpResponse back

--- src/main/scala/forex/interfaces/api/utils/ApiExceptionHandler.scala
+++ src/main/scala/forex/interfaces/api/utils/ApiExceptionHandler.scala
@@ -1,6 +1,7 @@
 package forex.interfaces.api.utils
 
 import akka.http.scaladsl._
+import akka.http.scaladsl.model.{ HttpResponse, StatusCodes }
 import forex.processes._
 import io.circe.syntax._
 
@@ -10,16 +11,30 @@ object ApiExceptionHandler {
     server.ExceptionHandler {
       case RatesError.Generic ⇒
         ctx ⇒
-          ctx.complete("Something went wrong in the rates process")
+          ctx.complete(
+            HttpResponse(
+              status = StatusCodes.InternalServerError,
+              entity = "Something went wrong in the rates process"
+            )
+          )
       case RatesError.System(underlying) ⇒
-          ctx ⇒
-          ctx.complete(underlying.getLocalizedMessage)
+        ctx ⇒
+          ctx.complete(HttpResponse(
+            status = StatusCodes.InternalServerError,
+            entity = underlying.getLocalizedMessage
+          ))
       case err: RatesError.Api ⇒
-          ctx ⇒
-          ctx.complete(err.asJson.toString())
+        ctx ⇒
+          ctx.complete(HttpResponse(
+            status = StatusCodes.InternalServerError,
+            entity = err.asJson.toString()
+          ))
       case _: Throwable ⇒
         ctx ⇒
-          ctx.complete("Something else went wrong")
+          ctx.complete(HttpResponse(
+            status = StatusCodes.InternalServerError,
+            entity = "Something else went wrong"
+          ))
     }
 
 }
